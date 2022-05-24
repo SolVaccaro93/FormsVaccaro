2e72a2b411b6bd61f14b29d9c39c7141f2cf43fb
Sol Vaccaro <vaccaro.marisol93@gmail.com>
2022-05-23 21:07:42 -0300

    .

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/forms-perez'),
+      dir: require('path').join(__dirname, './coverage/forms-vaccaro'),
       subdir: '.',
       reporters: [
         { type: 'html' },
